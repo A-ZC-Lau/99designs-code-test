3dc1878e27283c0ab32d5b8a632757079086def3
Chris Campbell <chris@harto.org>
2018-08-23 10:55:03 +1000

    Initial files

---
+++ js/movie-reviews-app.js
@@ -0,0 +1,13 @@
+const MovieReviewsApp = function(reviews, movies) {
+  let tweets = [];
+
+  // Put your code here !
+
+  return {
+    getTweets: function() {
+      return tweets;
+    }
+  };
+};
+
+module.exports = MovieReviewsApp;
