0225f9f4cd484b11028fd1ce5be4e5fdc203870d
Alexander Lau <alexanderlau@Alexanders-MacBook-Pro.local>
2018-09-11 22:09:02 +1000

    [movie-reviews-app.js] added createTweet function

--- js/movie-reviews-app.js
+++ js/movie-reviews-app.js
@@ -2,6 +2,18 @@ const MovieReviewsApp = function(reviews, movies) {
   let tweets = [];
 
   // Put your code here !
+  for (let review of reviews)
+  {
+    let movie = movies.find( movie => movie.title === review.title )
+
+    let tweet =
+      createTweet({
+        title: review.title,
+        year: movie.year || null,
+        review: review.review,
+        score: review.score
+      })
+  }
 
   return {
     getTweets: function() {
@@ -9,5 +21,19 @@ const MovieReviewsApp = function(reviews, movies) {
     }
   };
 };
+
+function createTweet({
+  title,
+  year,
+  review,
+  score
+})
+{
+  let score = Math.round(score / 10) * 10
+  let full_stars = "★".repeat(Math.round(score/2))
+  let half_star = score % 2 ? "½" : ""
+  let stars = full_stars + half_star
+  return `${title} ${year}: ${review} ${stars}`
+}
 
 module.exports = MovieReviewsApp;
