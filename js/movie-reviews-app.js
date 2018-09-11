91f855d8417552ad8ea1406c04fd58264b0c7ed6
Alexander Lau <alexanderlau@Alexanders-MacBook-Pro.local>
2018-09-11 22:53:25 +1000

    [movie-reviews-app.js] added payload  variable for each management, tweet now correctly shortens (tested)

--- js/movie-reviews-app.js
+++ js/movie-reviews-app.js
@@ -1,5 +1,6 @@
 const {
-  createTweet
+  createTweet,
+  createStars
 } = require("./functions.js")
 
 const MovieReviewsApp = function(reviews, movies) {
@@ -10,13 +11,37 @@ const MovieReviewsApp = function(reviews, movies) {
   {
     let movie = movies.find( movie => movie.title === review.title )
 
-    let tweet =
-      createTweet({
-        title: review.title,
-        year: movie && movie.year || null,
-        review: review.review,
-        score: review.score
-      })
+    let payload = {
+      review: review.review,
+      score: review.score,
+      title: review.title,
+      year: movie && movie.year || null,
+    }
+
+    let tweet = createTweet(payload)
+    // shorten title to 25
+    if (tweet.length > 140)
+    {
+      payload.title = payload.title.slice(0, 25)
+      tweet = createTweet(payload)
+    }
+    
+    // if tweet is still greater than 140, then shorten review
+    if (tweet.length > 140)
+    {
+      // get stars for appending
+      let stars = createStars(payload.score)
+      if (stars.length)
+      {
+        // extra 1 for space
+        tweet = tweet.slice(0, 140-stars.length-1) + " " + stars
+      }
+      // if no stars, then no extra trimming needed
+      else
+      {
+        tweet = tweet.slice(0, 140)
+      }
+    }
 
     tweets.push(tweet)
   }
